ccdeb0a795a1770c416698d1d82741947b3aef99
PitrPi <pitrpicko@gmail.com>
2020-12-17 19:01:53 +0100

    202016 Part 2 better documented, so it is at least bit understandable

--- y2020/task_16/16.py
+++ y2020/task_16/16.py
@@ -74,13 +74,21 @@ if __name__ == '__main__':
             valid_tickets.append(ticket)
 
     def decide_fields(valid_tickets, instructions):
+        """Given valid tickets and restrictions in instructions
+        decide which positions could be filled be which field
+        Returns list of possible field names for each index
+        """
+        # Start with transposing. So transposed[0] is first values in each ticket
         transposed = list(map(list, zip(*valid_tickets)))
         possible_fields_all = []
         for field_values in transposed:
+            # For every value on first position check validity in every possible field
             possible_fields = []
             for instruction_nm, instruction_val in instructions.items():
                 if instruction_nm == 'global_mins' or instruction_nm == 'global_maxs':
                     continue
+                # If all values for given fields satisfy requirement for given field
+                # add name of that field to possible fields
                 if all([
                         check_value_in_range(
                         int(value), instruction_val['mins'], instruction_val['maxs'])
@@ -91,21 +99,33 @@ if __name__ == '__main__':
         return possible_fields_all
 
     def reduce_fields(possible_fields, final=None):
+        """
+        This eliminates possibilities from decide_fields
+        :param possible_fields: What is still possible
+        :param final: this dictionary contains solved elements. Key=name of field, value=index on ticket
+        :return:
+        """
         if final is None:
             final = {}
         final_len = len(final)
-        # Map
+        # Map - find trivial pairings
+        # If given index has only one possible name assing this to final
         for idx, possible_field in enumerate(possible_fields):
             if len(possible_field) == 1:
                 final[possible_field[0]] = idx
         # Reduce
+        # From possible fields remove field names, that was already assigned in final
+        # This also ensures that we do not double assign, since possible fields for solved
+        # indexes will be empty
         possible_fields = [[name
                             for name
                             in field
                             if name not in final.keys()]
                            for field in possible_fields]
+        # If we made any progress, call this function again and further reduce the possibilities
         if len(final) > final_len:
             reduce_fields(possible_fields, final)
+        # Once there are no trivial solutions return final
         return possible_fields, final
 
 
